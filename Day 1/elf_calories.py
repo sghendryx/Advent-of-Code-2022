b10def168d6ed7573d3f09b7f9bce5bb8548762c
Solveig Hendryx <114836976+sghendryx@users.noreply.github.com>
2022-12-01 11:15:10 -0700

    Update elf_calories.py

--- Day 1/elf_calories.py
+++ Day 1/elf_calories.py
@@ -12,7 +12,7 @@ def main():
     all_calorie_totals = []
     
     for food_item in list_of_strings:
-        # For every item, it takes that string value and turns it into an integer, and adds it to the list
+        # For every food_item, it takes that string value and turns it into an integer, and adds it to the list
         # calories_per_elf.
         if bool(food_item) == True:
             item = int(food_item)
